f112ccce53bcddcda9454fcad1ffc3a3c9ba238e
tommy-012 <lonnlilonn@googlemail.com>
2024-02-17 17:32:08 -0800

    雛型の作成

---
+++ lib/wordnet_japan_diagram.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wordnet_japan_diagram/version"
+
+module WordnetJapanDiagram
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/wordnet_japan_diagram_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WordnetJapanDiagram do
+  it "has a version number" do
+    expect(WordnetJapanDiagram::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
